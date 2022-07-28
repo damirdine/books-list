40e51e0650c2b5b938e242c600fb6ec8a3b0a8c9
damirdine <damirdine@outlook.fr>
2022-07-28 14:07:15 +0200

    get authors ✅

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+let selectAuthors = document.querySelector('#authors')
+
 fetch('books.json')
 .then(res => res.json())
 .then(json => app(json))
 
 function app(data){
-    let Authors = getAuthors(data)
-    console.log(Authors)
+    let allData = data
+    let authors = getAuthors(allData)
+    displaySelectAuthors(authors.sort())
 }
 
 function getAuthors(data){
     let Allbooks = data
     let listAuthors = []
-    
+
     Allbooks.forEach(element => {
        let authors = element.authors
        authors.forEach(author=>{
@@ -21,4 +24,12 @@ function getAuthors(data){
 
     });
     return listAuthors
+}
+function displaySelectAuthors(authors){
+    let authorsList = authors
+    authorsList.forEach(author =>{
+        if(author!=""){
+            selectAuthors.innerHTML += `<option value="${author}">${author}</option>`
+        }
+    })
 }
